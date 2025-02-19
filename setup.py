04874841bab6149023ae5b1d3b0d6b64e8a50ca0
Kiran Singh <2241018@sliet.ac.in>
2025-02-19 15:51:26 +0530

    editable mode update

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import find_packages,setup
 # from typing import list 
 
+def get_requirements() -> list[str]:
+    
+    requirements_list= list[str]= []
+    
+    return requirements_list
+
 setup(
     name="sensor",
     version="0.0.1",
     author="Kiran Singh",
     author_email="2241018@sliet.ac.in",
     packages=find_packages(),
-    install_requires=["pymongo"]
+    install_requires=get_requirements() ,#["pymongo"]
 )
